236c4565006a15924c276d7de5de607a61912ed7
Kevin Hasalava <165575533+KevinHasalava@users.noreply.github.com>
2026-06-11 23:58:59 +0530

    feat: wrap application with GoogleOAuthProvider in index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID || "968919350255-ch6qibth61d0rnel6cu6vcutu9o046o0.apps.googleusercontent.com"}>
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
